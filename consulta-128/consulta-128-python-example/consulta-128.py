8405218a5afe5177e48c47d081aeb8cabb128f65
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-07-14 10:08:24 -0300

    Exemplo em Python da consulta 128

---
+++ consulta-128/consulta-128-python-example/consulta-128.py
@@ -0,0 +1,18 @@
+import http.client
+
+conn = http.client.HTTPConnection("186.249.34.34")
+
+payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\ntom\r\n-----011000010111000001101001--"
+
+headers = {
+    'content-type': "multipart/form-data; boundary=---011000010111000001101001",
+    'authorization': "Basic ZW50ZXJwbHVnOnBsdWdlbnRlcg==",
+    'cache-control': "no-cache",
+    }
+
+conn.request("GET", "/consulta/?email=consulta%40enterplug.com.br&senha=consulta&codigoProduto=128&tipoConsumidor=F&documentoConsumidor=42924057191", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

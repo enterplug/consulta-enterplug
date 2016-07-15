fe263ce1763324d47715553f629d3927598fbacd
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-07-15 09:32:06 -0300

    Criado exemplo da consulta 4 em go

---
+++ consulta-4/consulta-4-go-example/consulta-4-go-example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+
+	url := "http://186.249.34.34/consulta/?email=henrique%40enterplug.com.br&senha=enterplug&codigoProduto=4&tipoConsumidor=F&documentoConsumidor=42924057191"
+
+	payload := strings.NewReader("-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\ntom\r\n-----011000010111000001101001--")
+
+	req, _ := http.NewRequest("GET", url, payload)
+
+	req.Header.Add("content-type", "multipart/form-data; boundary=---011000010111000001101001")
+	req.Header.Add("authorization", "Basic ZW50ZXJwbHVnOnBsdWdlbnRlcg==")
+	req.Header.Add("cache-control", "no-cache")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
